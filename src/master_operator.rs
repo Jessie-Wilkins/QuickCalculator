81e1a0c48b5a568a5f8b4409d9bdbe0c9668dbc5
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2021-03-29 07:14:03 -0500

    Created input processor using all operators

--- src/master_operator.rs
+++ src/master_operator.rs
@@ -24,6 +24,26 @@ mod multi_operations {
         answer_string += &("Remainder: ".to_string()+&tuple_answers.4.to_string());
         answer_string
     }
+
+    pub fn convert_strings_to_ints(first: &str, second: &str) -> [i32; 2] {
+        
+        [first.to_string().parse::<i32>().unwrap(), second.to_string().parse::<i32>().unwrap()]
+    }
+
+    pub fn is_input_a_number(input : &str) -> bool {
+        input.chars().all(char::is_numeric)
+    }
+
+    pub fn multi_input_basic_calculation_processor(first: &str, second: &str) -> String {
+        if is_input_a_number(first) && is_input_a_number(second) {
+            let int_array = convert_strings_to_ints(first, second);
+            let answer_tuple = calculate_basics(int_array[0], int_array[1]);
+            multi_answer_printer(answer_tuple)
+        }
+        else {
+            "The inputs given were not integers. Please input integers.".to_string()
+        }
+    }
 }
 
 
@@ -46,6 +66,24 @@ mod tests {
     #[test]
     fn multi_answer_printer_prints_all_answers_including_ones_with_decimals() {
         let formatted_answer = multi_answer_printer(calculate_basics(5, 2));
+        assert_eq!(formatted_answer, "Sum: 7\nDifference: 3\nProduct: 10\nQuotient: 2.5\nRemainder: 1")
+    }
+
+    #[test]
+    fn multi_user_strings_are_converted_to_numbers() {
+        let integer_array = convert_strings_to_ints("4", "2");
+        assert_eq!(integer_array, [4, 2]);
+    }
+
+    #[test]
+    fn multi_user_input_validation_returns_false_when_input_is_not_a_number() {
+        let is_number = is_input_a_number("t");
+        assert_eq!(is_number, false);
+    }
+
+    #[test]
+    fn multi_user_input_processing_returns_string_answers() {
+        let formatted_answer = multi_input_basic_calculation_processor("5", "2");
         assert_eq!(formatted_answer, "Sum: 7\nDifference: 3\nProduct: 10\nQuotient: 2.5\nRemainder: 1")
     }
 }
