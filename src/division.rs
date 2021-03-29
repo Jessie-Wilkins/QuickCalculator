231ba139cd8da8f943a274034bc4fe5f7c8deec1
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2021-03-29 07:35:40 -0500

    Created check for division by 0

--- src/division.rs
+++ src/division.rs
@@ -1,7 +1,12 @@
 #[allow(dead_code)]
 pub fn divide_two(first: i32, second: i32) -> f32{
     let quotient : f32 = first as f32 / second as f32;
+    if second == 0 {
+       0.0
+    }
+    else {
     quotient
+    }
 }
 
 #[cfg(test)]
@@ -23,5 +28,11 @@ mod tests {
     fn division_method_correctly_divides_parameters_that_gives_non_whole_quotient() {
         let quotient = divide_two(5, 2);
         assert_eq!(quotient, 2.5)
+    }
+
+    #[test]
+    fn division_method_does_not_calculate_something_divided_by_zero() {
+        let quotient = divide_two(2, 0);
+        assert_eq!(quotient, 0.0);
     }
 }
